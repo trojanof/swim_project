e55b27af07fd898e031d93c6a14601f6551e025a
Vsevolod <vsevolodtroy@gmail.com>
2025-01-29 23:52:48 +0500

    credentials reading update for streamlit

--- parse_sheets.py
+++ parse_sheets.py
@@ -1,9 +1,29 @@
 '''Module for google sheets parsing'''
+import json
+from tempfile import TemporaryDirectory
+from pathlib import Path
+import streamlit as st
 from googleapiclient.discovery import build
 from oauth2client.service_account import ServiceAccountCredentials
 import httplib2
 import pandas as pd
 from settings import CREDENTIALS, SCOPES, FILE_ID, CELL_RANGE
+
+
+def get_service_from_var():
+    cred_str = st.secrets['CREDS']
+    creds_obj = json.loads(cred_str)
+    tmp_dir = TemporaryDirectory()
+    tmp_dir_path = Path(tmp_dir.name)
+    json_path = tmp_dir_path / 'creds.json'
+    with open(json_path, 'w') as f:
+        json.dump(creds_obj, f)
+    creds_service = ServiceAccountCredentials.from_json_keyfile_name(
+        CREDENTIALS,
+        SCOPES
+        ).authorize(httplib2.Http())
+    tmp_dir.cleanup()
+    return build('sheets', 'v4', http=creds_service)
 
 
 def get_service_acc():
@@ -15,7 +35,7 @@ def get_service_acc():
 
 
 def get_df_from_google_sheet(sheet_name):
-    data = get_service_acc().spreadsheets().values().get(
+    data = get_service_from_var().spreadsheets().values().get(
         spreadsheetId=FILE_ID,
         range=f"{sheet_name}!{CELL_RANGE}").execute()
     df = pd.DataFrame(data['values'], columns=data['values'][0])
